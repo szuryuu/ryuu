98585c7d6524e318a2bda71e0458c56a2da77a4d
shfwnz <ilhamdzaky2007@gmail.com>
2025-07-16 13:18:40 +0700

    fix: home ui

---
+++ components/hero.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useMediaQuery } from "@vueuse/core";
+
+const isDesktop = useMediaQuery("(min-width: 768px)");
+
+const gradientClass = computed(() =>
+  isDesktop.value
+    ? "bg-[radial-gradient(circle_400px_at_50%_300px,#2E2E2E,transparent)]"
+    : "bg-[radial-gradient(circle_500px_at_50%_400px,#2E2E2E,transparent)]",
+);
+</script>
+
+<template>
+  <div
+    class="absolute top-0 right-0 bottom-0 -z-10 left-0 flex justify-center items-center"
+    :class="gradientClass"
+  >
+    <div class="flex items-start gap-0">
+      <div
+        class="upside dark:text-white text-2xl font-bold font-decoration flex-shrink-0"
+      >
+        ポルトフォリオ
+      </div>
+      <span class="font-display [writing-mode:vertical-lr]">(portofolio)</span>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import Hero from "~/components/hero.vue";
+
 const { data: home } = await useAsyncData(() =>
   queryCollection("content").path("/").first(),
 );
@@ -10,6 +12,7 @@ useSeoMeta({
 </script>
 
 <template>
+  <Hero />
   <main class="px-6 flex-1 items-end flex">
     <!-- <ContentRenderer
       v-if="home"
@@ -18,11 +21,22 @@ useSeoMeta({
     />
     <div v-else class="text-red-500">Home not found</div> -->
 
-    <div class="py-2">
-      <h5 class="pl-2 py-4 uppercase font-display text-xs">
-        full stack developer / cyber security / devops
-      </h5>
-      <h1 class="text-9xl font-display">Szuryu Ryujin</h1>
+    <div
+      class="py-2 justify-between w-full items-end grid grid-cols-1 md:grid-cols-3"
+    >
+      <div class="md:col-span-2">
+        <h5 class="pl-2 py-4 uppercase font-display text-xs">
+          full stack developer / cyber security / devops
+        </h5>
+        <h1 class="text-9xl font-display">Szuryu Ryujin</h1>
+      </div>
+      <div
+        class="text-xs overflow-hidden font-display md:col-span-1 md:block hidden text-right py-4"
+      >
+        Full Stack Developer, Cyber Security Engineer, and DevOps Practitioner
+        focused on creating optimal software solutions that balance
+        functionality with user experience.
+      </div>
     </div>
   </main>
 </template>
