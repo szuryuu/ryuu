66b05b6edb5202a0dc4ec458bf163ecaf8315357
shfwnz <ilhamdzaky2007@gmail.com>
2025-07-20 19:05:23 +0700

    fix: ui

--- pages/about.vue
+++ pages/about.vue
@@ -5,10 +5,8 @@ import { gsap, ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 
 function initTriggers() {
-  // Set z-index
   gsap.set(".panel", { zIndex: (i, target, targets) => targets.length - i });
 
-  // Create animation
   gsap.to(".panel:not(:last-child)", {
     yPercent: -100,
     ease: "none",
@@ -19,7 +17,6 @@ function initTriggers() {
       end: "+=300%",
       scrub: true,
       pin: true,
-      markers: true,
     },
   });
 }
@@ -33,29 +30,36 @@ onMounted(async () => {
   }, 100);
 });
 
-onUnmounted(() => {
-  ScrollTrigger.getAll().forEach((t: any) => t.kill());
-});
+onUnmounted(() => {});
 </script>
 
 <template>
-  <div id="panel-container" class="absolute top-0 left-0 w-full h-full">
+  <div id="panel-container" class="w-full bg-yellow-500">
     <section class="panel bg-red-500">
       <div class="text-6xl font-bold text-white">Panel 1</div>
     </section>
     <section class="panel bg-yellow-400">
       <div class="text-6xl font-bold text-white">Panel 2</div>
     </section>
-    <section class="panel bg-yellow-400">
-      <div class="text-6xl font-bold text-white">Panel 3</div>
+    <section class="panel bg-red-500">
+      <div class="text-6xl font-bold text-white">Panel 1</div>
     </section>
     <section class="panel bg-yellow-400">
-      <div class="text-6xl font-bold text-white">Panel 3</div>
+      <div class="text-6xl font-bold text-white">Panel 2</div>
     </section>
   </div>
 </template>
 
 <style scoped>
+#panel-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+}
+
 .panel {
   position: absolute;
   will-change: transform;
