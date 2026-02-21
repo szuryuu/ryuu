f9de89ec83b6b612a1cfa45577887e657aadf3ff
szuryuu <ilhamdzaky2007@gmail.com>
2026-02-21 17:25:41 +0700

    feat: icons

--- assets/images/icons/index.ts
+++ assets/images/icons/index.ts
@@ -3,5 +3,6 @@ import Github2 from "./github2.svg";
 import Mail from "./gmail.svg";
 import Linkedin from "./linkedin.svg";
 import FileText from "./file-text.svg";
+import Pen from "./pen.svg";
 
-export { Github, Github2, Mail, Linkedin, FileText };
+export { Github, Github2, Mail, Linkedin, FileText, Pen };
