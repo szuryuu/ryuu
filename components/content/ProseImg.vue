12e2a1eab757eb5076c6f16e2ef0064cbed4928a
szuryuu <ilhamdzaky2007@gmail.com>
2026-03-19 11:08:38 +0700

    fix: prose image

---
+++ components/content/ProseImg.vue
@@ -0,0 +1,22 @@
+<template>
+  <figure class="my-8">
+    <img
+      :src="src"
+      :alt="alt"
+      class="w-full rounded-lg border border-white/10 object-cover"
+    />
+    <figcaption
+      v-if="alt"
+      class="text-xs text-white/40 font-display text-center mt-2"
+    >
+      {{ alt }}
+    </figcaption>
+  </figure>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  src: string;
+  alt?: string;
+}>();
+</script>
